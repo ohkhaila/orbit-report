d9719e6ece44a67254dfa653bbce6fb72026808e
ohkhaila <khaila.jones@lessannoyingcrm.com>
2020-04-14 23:55:02 -0500

    Step 4 Display Table of Satellites Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
-  sourceList: Satellite[];
+  sourceList:Satellite[];
   constructor() {
     this.sourceList = [
        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
